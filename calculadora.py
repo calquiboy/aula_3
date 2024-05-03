1b360fc7da2faf8ee415da594b5d8b539c82e205
Pedro Calqui <168238157+calquiboy@users.noreply.github.com>
2024-05-03 19:21:40 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+
+n1 = int(input('>>'))
+n2 = int(input('>>'))
+
+soma = n1 + n2
+
+div = n1 / n2
+
+multi = n1 * n2
+
+sub = n1 - n2
+
+print('Soma' + soma)
+
+print('Subtração' + sub)
+
+print('Multiplicação' + multi)
+
+print('Divisão' + div)
